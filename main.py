353783fd169408e65134b3c23ee906f922398d94
Kyle <kyle@pykyle.tech>
2020-10-25 15:11:33 +0000

    Music + Plex
    Lists the trees of both music and plex shares and saves them to a csv.

--- main.py
+++ main.py
@@ -4,16 +4,35 @@
 ## import module to traverse the filesystem
 import os
 ## path to directory to list + write files, this case an UNRAID share
-path = "/Users/kyle/Dropbox/My Mac (Kyles-MacBook-Pro.local)/Desktop"
-## csv file to write to
-music_csv = 'music.csv'
-## create / open a csv file 
+m_path = "/Volumes/SHARE/KYLE/MUSIC"
+## path to plex share
+p_path = "/Volumes/SHARE/MEDIANEW"
+## csv files to write to
+music_csv = 'csv/music.csv'
+plex_csv = 'csv/plex.csv'
 
-with open(music_csv, 'w') as output:
-    output.write('FILE NAME,FOLDER NAME,FILE PATH,\n')
-    for root, dirs, files in os.walk(path):
-        for x in files:
-            file_path= os.path.join(root, x)
-            folder_name = os.path.dirname(root)
-            file_name= os.path.basename(x)
-            output.write('{},{},{}\n'.format(file_name, folder_name, file_path))
+def music_share():
+    ## create / open music csv file
+    with open(music_csv, 'w') as output:
+        output.write('FILE NAME,FOLDER NAME,FILE PATH,\n')
+        for root, dirs, files in os.walk(m_path):
+            for x in files:
+                file_path= os.path.join(root, x)
+                folder_name = os.path.dirname(root)
+                file_name= os.path.basename(x)
+                output.write('{},{},{}\n'.format(file_name, folder_name, file_path))
+
+def plex_share():
+    ## create / open plex csv file
+    with open(plex_csv, 'w') as output:
+        output.write('FILE NAME,FOLDER NAME,FILE PATH,\n')
+        for root, dirs, files in os.walk(p_path):
+            for x in files:
+                file_path= os.path.join(root, x)
+                folder_name = os.path.dirname(root)
+                file_name= os.path.basename(x)
+                output.write('{},{},{}\n'.format(file_name, folder_name, file_path))
+
+## call functions to run program
+music_share()
+plex_share()
